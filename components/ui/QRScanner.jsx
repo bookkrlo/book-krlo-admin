1d5d311c4f85f09069198c0aa1ca55c4496f737d
bookkrlo <bookkrlo.info@gmail.com>
2024-11-26 01:16:40 +0500

    Update QRScanner.jsx

--- components/ui/QRScanner.jsx
+++ components/ui/QRScanner.jsx
@@ -49,7 +49,7 @@ export function QRScanner({ isOpen, onClose, onScanSuccess }) {
                 </DialogHeader>
                 {isScanning && (
                     <Scanner
-                        onResult={handleScan}
+                        onScan={handleScan}
                         onError={handleError}
                         constraints={{ facingMode: 'environment' }}
                     />
