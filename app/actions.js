97c972c4617b041deb695d0b565f394a8f776fc0
bookkrlo <bookkrlo.info@gmail.com>
2024-11-26 01:07:52 +0500

    Update actions.js

--- app/actions.js
+++ app/actions.js
@@ -3,16 +3,22 @@
 import connectDB from '@/config/database';
 import Guest from '@/models/Guest';
 
-export async function updateTicketStatus(id, isTicketUsed) {
+export async function updateTicketStatus(ticketNumber, isTicketUsed) {
     try {
         await connectDB();
-        const guest = await Guest.findByIdAndUpdate(
-            id,
-            { isTicketUsed },
-            { new: true }
+        const guest = await Guest.findOneAndUpdate(
+            { ticket_number: ticketNumber }, // Query by ticket_number
+            { isTicketUsed }, // Update the isTicketUsed field
+            { new: true } // Return the updated document
         ).lean();
+
+        if (!guest) {
+            return { success: false, error: 'Guest not found' };
+        }
+
         return { success: true, guest: JSON.parse(JSON.stringify(guest)) };
     } catch (error) {
+        console.error('Error updating ticket status:', error);
         return { success: false, error: 'Failed to update ticket status' };
     }
 }
